489b1c282cd4a04fde3e37dfa7e06bb6b65bfd98
xycarto <xycarto@gmail.com>
2023-04-21 17:14:56 +1200

    run with variable

--- utils/build-infra.sh
+++ utils/build-infra.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source ../.creds
+# bash build-infra.sh "CA_SantaClaraCounty_2020"
+
+WORKUNIT=$1
+
+source .creds
 
 cd terraform
 
@@ -11,12 +15,14 @@ terraform apply -auto-approve
 terraform validate
 
 # Add instance description and test here
-aws ec2 wait instance-running --region "us-west-2" --instance-ids $(terraform output -raw instance_id) 
+aws ec2 wait instance-status-ok --region "us-west-2" --instance-ids $(terraform output -raw instance_id) 
+
+sleep 180
 
 scp -o StrictHostKeyChecking=no -i ${key} -r ../.creds ubuntu@$(terraform output -raw instance_public_ip):/home/ubuntu/
 
 scp -o StrictHostKeyChecking=no -i ${key}  -r ../run-index.sh ubuntu@$(terraform output -raw instance_public_ip):/home/ubuntu/
 
-ssh -o StrictHostKeyChecking=no -i ../.ssh/wesm-pair.pem  ubuntu@$(terraform output -raw instance_public_ip) "bash run-index.sh"
+ssh -o StrictHostKeyChecking=no -i ../.ssh/wesm-pair.pem  ubuntu@$(terraform output -raw instance_public_ip) "bash run-index.sh $WORKUNIT"
 
-# terraform destroy -auto-approve
+terraform destroy -auto-approve
--- utils/run-index.sh
+++ utils/run-index.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# bash run-index.sh
+# bash run-index.sh "CA_SantaClaraCounty_2020"
 
 source .creds
+
+WORKUNIT=$1
 
 git clone https://${TOKEN}@github.com/xycarto/wesm-grids.git
 
@@ -10,8 +12,10 @@ cp -r .creds wesm-grids/utils/src/
 
 cd wesm-grids/utils/src
 
-sudo make docker-local
+echo ${DOCKER_PW} | sudo docker login --username deployhub1 --password-stdin
 
-sudo make wesm-index workunit="CA_SantaClaraCounty_2020"
+sudo make docker-pull
+
+sudo make wesm-index workunit=$WORKUNIT
 
 exit
